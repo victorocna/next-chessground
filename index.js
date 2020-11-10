7ca166f23275cbba41f1a07cf6794de177d2a8f6
victorocna <victorocnarescu@gmail.com>
2020-11-11 00:28:17 +0200

    add pieces css

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Chessground } from './components';
 import './public/css/chess.css';
 import './public/css/coordinates.css';
+import './public/css/pieces.css';
 
 export default Chessground;
 export { Chessground };
