bb5cd0ef06b04e1857bd88d98cf4aabf3a9fb600
victorocna <victorocnarescu@gmail.com>
2021-12-06 14:51:11 +0200

    updated code sample

--- examples/utils/code-samples/play.js
+++ examples/utils/code-samples/play.js
@@ -1,9 +1,33 @@
 const play = `import NextChessground from 'next-chessground';
 
+import Stockfish from '../../utils/stockfish';
+import engineMove from '../../utils/engine-move';
+
 const Page = () => {
-  return (
-    <NextChessground />
-  );
+  const ref = useRef();
+
+  const [engine] = useState(new Stockfish());
+  useEffect(() => {
+    engine.init();
+  }, []);
+
+  const [engineTurn, setEngineTurn] = useState(true);
+  const onMove = async (chess) => {
+    setEngineTurn((prev) => !prev);
+
+    if (engineTurn) {
+      if (chess.game_over()) {
+        engine.quit();
+      }
+
+      await engine.set_position(chess.fen());
+      const move = engineMove(await engine.go_time(1000));
+
+      ref.current.board.move(move.from, move.to);
+    }
+  };
+
+  return <NextChessground ref={ref} onMove={onMove} />;
 };
 
 export default Page;`;
