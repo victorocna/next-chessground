9fd5bcfe6a03bff930222a3aca6358c0aa3b50d7
victorocna <victorocnarescu@gmail.com>
2021-12-06 18:23:16 +0200

    update stockfish

--- utils/stockfish.js
+++ utils/stockfish.js
@@ -1,28 +1,20 @@
-/**
- * Constructor requires a path to stockfish to run correctly in a web browser
- */
+import constants from './constants';
 
-const DEFAULT_PATH = 'http://localhost:3000/stockfish.asm.js';
-const DEFAULT_SKILL = 20;
-const DEFAULT_CP_TOLERANCE = 25;
-const DEFAULT_MATE_TOLERANCE = 0;
-
-class Engine {
-  constructor(props) {
+class Stockfish {
+  constructor() {
     if (typeof window === 'undefined') {
       return false;
     }
-    const { path = DEFAULT_PATH, skill = DEFAULT_SKILL, onInfoMessage } =
-      props || {};
+
     if (!window.chessEngineWorker) {
-      window.chessEngineWorker = new Worker(path);
+      const worker = 'https://next-chessground.vercel.app/stockfish.asm.js';
+      window.chessEngineWorker = new Worker(worker);
     }
-    this.onInfoMessage = onInfoMessage;
-    this.skillLevel = skill;
+
+    this.skillLevel = 20;
     this.resolveTimeout = null;
     this.isAnalyzing = false;
-    this.fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-    this.MAX_DEPTH = 40;
+    this.fen = constants.initialFen;
   }
   getTurnFromFen(fen) {
     return fen.split(' ')[1];
@@ -30,7 +22,6 @@ class Engine {
   normalizeScoreValue(value) {
     const { fen } = this;
     const turn = this.getTurnFromFen(fen);
-    // console.log('turn is ', turn, '& value is ', value);
     if (turn === 'b') {
       return -1 * value;
     }
@@ -48,11 +39,6 @@ class Engine {
      * skill level maximum error is 0-5000, lower the stronger
      * skill level probability is 1-1000, the higher the stronger
      * seems to be working with max value for maxError and min value for probability
-     */
-    /**
-     * OLD WAY OF COMPUTING SKILL LEVEL VALUES;
-     * const maxError = (20 - this.skillLevel) * 250;
-     * const probability = 1000 - (20 - this.skillLevel) * 50;
      */
     const maxError = 5000;
     const probability = 1;
@@ -99,11 +85,6 @@ class Engine {
   }
 
   parseData(data) {
-    /**
-     * info depth 21 seldepth 30 multipv 1 score cp 9 nodes 2733558 nps 401050 hashfull 864 time 6816 pv e6d5 c1f4 f8d6 f4d6 d8d6 e2e3 c8f5 g1e2 b8d7 e2g3 f5g6 f1d3 c7c6 e1g1 e8g8 d3g6 h7g6 h2h3 a8c8 a1c1 f8e8 d1b3 c8b8 c3e2 g8h7 bmc 1.86128e-06
-     */
-    // return data;
-    console.log(data);
     const depth = data.split(' ')[2];
     let pv = '';
     if (data.indexOf(' pv ') > -1) {
@@ -138,19 +119,11 @@ class Engine {
     });
   }
   set_position(fen) {
-    console.log('setting position ', fen);
     return new Promise((resolve) => {
       this.fen = fen;
       window.chessEngineWorker.postMessage('position fen ' + fen);
       resolve();
     });
-  }
-
-  async new_position(fen) {
-    await this.stop();
-    await this.is_ready();
-    this.set_position(fen);
-    this.go_infinite();
   }
 
   set_multipv(numPv) {
@@ -198,59 +171,11 @@ class Engine {
     return new Promise((resolve) => {
       window.chessEngineWorker.postMessage('go movetime ' + time);
       window.chessEngineWorker.onmessage = (message) => {
-        if (
-          this.isInfoMessage(message) &&
-          typeof this.onInfoMessage === 'function'
-        ) {
-          this.onInfoMessage(message.data);
-        }
         if (message.data.startsWith('bestmove')) {
           resolve(message.data);
         }
       };
     });
-  }
-
-  go_infinite() {
-    window.chessEngineWorker.postMessage('go infinite');
-    this.isAnalyzing = true;
-    window.chessEngineWorker.onmessage = (message) => {
-      if (
-        this.isInfoMessage(message) &&
-        typeof this.onInfoMessage === 'function'
-      ) {
-        const msgData = this.parseData(message.data);
-        const { depth } = msgData;
-        if (depth > this.MAX_DEPTH) {
-          this.stop();
-        }
-        this.onInfoMessage(msgData);
-      }
-    };
-  }
-
-  async isSimilarEval({ firstFen, secondFen, depth, tolerance }) {
-    const firstScore = await this.get_score(firstFen, depth);
-    const secondScore = await this.get_score(secondFen, depth);
-    if (firstScore.type !== secondScore.type) {
-      return false;
-    }
-
-    if (firstScore.type === 'cp') {
-      const acceptedCpTolernace =
-        (tolerance && tolerance.cp) || DEFAULT_CP_TOLERANCE;
-
-      return (
-        Math.abs(firstScore.value - secondScore.value) <= acceptedCpTolernace
-      );
-    } else {
-      const acceptedMateTolerance =
-        (tolerance && tolerance.mate) || DEFAULT_MATE_TOLERANCE;
-
-      return (
-        Math.abs(firstScore.value - secondScore.value) <= acceptedMateTolerance
-      );
-    }
   }
   stop() {
     return new Promise((resolve) => {
@@ -267,11 +192,10 @@ class Engine {
       };
     });
   }
-
   quit() {
     window.chessEngineWorker.postMessage('quit');
   }
 }
 
-module.exports = Engine;
-module.exports.Stockfish = Engine;
+export default Stockfish;
+export { Stockfish };
