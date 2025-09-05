807b328e4c12b33ac26a9d27d273cf3ebfbd9243
victorocna <victorocnarescu@gmail.com>
2025-09-05 13:33:19 +0300

    Add classnames

--- components/Advanced.jsx
+++ components/Advanced.jsx
@@ -8,7 +8,7 @@ const Advanced = ({ flip, readOnly }) => {
   }
 
   return (
-    <div className="text-gray-400 flex flex-row-reverse gap-2 py-1.5">
+    <div className="chessground-footer text-gray-400 flex flex-row-reverse gap-2 py-1.5">
       <Settings />
       <Flip onClick={flip} />
     </div>
